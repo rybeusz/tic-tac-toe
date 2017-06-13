6e04c181580f35c9fc5d002eed7e921cbc71fcbf
Stefan Kania <stefankania95@gmail.com>
2017-06-13 15:18:33 +0200

    Gui

--- src/main/java/com/java/tictactoe/ui/Gui.java
+++ src/main/java/com/java/tictactoe/ui/Gui.java
@@ -2,11 +2,26 @@ package com.java.tictactoe.ui;
 
 import com.java.tictactoe.enums.Seed;
 
+import java.util.ArrayList;
+
 /**
  * Created by pati on 13.06.17.
  */
 public class Gui {
 
     public void showStartMessage(Seed currentPlayer) {
+        System.out.println(String.format("Current player: %s", currentPlayer));
+    }
+
+    public void showBoard(ArrayList<Seed> seeds) {
+        // | | | |
+        // | | | |
+        // | | | |
+    }
+
+
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+        gui.showStartMessage(Seed.CROSS);
     }
 }
