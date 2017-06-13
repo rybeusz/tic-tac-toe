2ecb280c28bfef7b38ab44e2dda8b61f72a9eabc
Stefan Kania <stefankania95@gmail.com>
2017-06-13 16:00:44 +0200

    UserInput works

--- src/main/java/com/java/tictactoe/ui/UserInput.java
+++ src/main/java/com/java/tictactoe/ui/UserInput.java
@@ -1,10 +1,16 @@
 package com.java.tictactoe.ui;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Created by pati on 13.06.17.
  */
 public class UserInput {
-    public Integer chooseCell() {
-        return null;
+    public Integer chooseCell() throws InputMismatchException {
+        Scanner scan = new Scanner(System.in);
+        Integer cellNumber = null;
+        cellNumber = scan.nextInt();
+        return cellNumber;
     }
 }
