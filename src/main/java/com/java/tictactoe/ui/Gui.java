a3d50d7235357174b48ef212b84026127bf8407f
Stefan Kania <stefankania95@gmail.com>
2017-06-13 15:39:44 +0200

    Add showBoard method in GUI

--- src/main/java/com/java/tictactoe/ui/Gui.java
+++ src/main/java/com/java/tictactoe/ui/Gui.java
@@ -14,17 +14,33 @@ public class Gui {
     }
 
     public void showBoard(ArrayList<Seed> seeds) {
-        // | | | |
-        // | | | |
-        // | | | |
+        int seedCount = 1;
+        String seedValue;
+        for (Seed seed : seeds) {
+            if (seed.equals(Seed.CROSS)) seedValue = "X";
+            else if (seed.equals(Seed.NOUGHT)) seedValue = "O";
+            else seedValue = " ";
+
+            System.out.print(String.format("|%s", seedValue));
+            if (seedCount == 3 || seedCount == 6 || seedCount == 9) System.out.print("|\n");
+            seedCount++;
+        }
     }
 
 
     public static void main(String[] args) {
         Gui gui = new Gui();
         gui.showStartMessage(Seed.CROSS);
-    }
-
-    public void showBoard(ArrayList<Seed> seeds) {
+        ArrayList<Seed> seeds = new ArrayList<>();
+        seeds.add(Seed.CROSS);
+        seeds.add(Seed.EMPTY);
+        seeds.add(Seed.CROSS);
+        seeds.add(Seed.NOUGHT);
+        seeds.add(Seed.NOUGHT);
+        seeds.add(Seed.EMPTY);
+        seeds.add(Seed.CROSS);
+        seeds.add(Seed.CROSS);
+        seeds.add(Seed.EMPTY);
+        gui.showBoard(seeds);
     }
 }
